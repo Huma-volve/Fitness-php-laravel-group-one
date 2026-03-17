7fd723fe190d56706dddb54c6a94617b58098e57
Wael Zaqout <waelzaqout4@gmail.com>
2026-03-17 17:32:56 +0200

    Review

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Auth\OtpController;
+use App\Http\Controllers\Api\LandingController;
+use App\Http\Controllers\Api\NewsletterController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -21,12 +25,22 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile',         [AuthController::class, 'profile']);
 
 
-Route::post('/forgot-password', [OtpController::class, 'sendOtp']);
-Route::post('/verify-otp',      [OtpController::class, 'verifyOtp']);
-Route::post('/reset-password',  [OtpController::class, 'resetPassword']);
-
-
+    Route::post('/forgot-password', [OtpController::class, 'sendOtp']);
+    Route::post('/verify-otp',      [OtpController::class, 'verifyOtp']);
+    Route::post('/reset-password',  [OtpController::class, 'resetPassword']);
 });
 
+Route::prefix('landing')->group(function () {
+    Route::get('/stats',    [LandingController::class, 'stats']);
+    Route::get('/trainers', [LandingController::class, 'trainers']);
+    Route::get('/packages', [LandingController::class, 'packages']);
+    Route::get('/reviews', [ReviewController::class, 'index']);
+    Route::get('/trainers/{trainerId}/reviews', [ReviewController::class, 'trainerReviews']);
+});
+
+Route::middleware('auth:sanctum')->prefix('landing')->group(function () {
+    Route::post('/newsletter', [NewsletterController::class, 'subscribe']);
+    Route::post('/reviews', [ReviewController::class, 'store']);
+});
 Route::get('/auth/google/redirect', [AuthController::class, 'googleRedirect']);
 Route::get('/auth/google/callback', [AuthController::class, 'googleCallback']);
